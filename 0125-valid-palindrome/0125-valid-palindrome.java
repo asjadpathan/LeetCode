d3a744ec5504a16fb904ab60e9e1cd0f3d9794d9
Asjad Pathan <146015297+asjadpathan@users.noreply.github.com>
2026-09-25 19:50:57 +0530

    Time: 2 ms (99.3%), Space: 44.2 MB (82.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = (s.length()) - 1;
 
-        while (left < right) {
-            if (!Character.isLetterOrDigit(s.charAt(left))) 
-                left++;
-            else if (!Character.isLetterOrDigit(s.charAt(right))) 
-                right--;
-            else if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) 
-                return false;
-            else {
-                left++; 
-                right--;
+        while( left< right ){
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left ++;
             }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right --;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }  
+
+            left ++;
+            right --;
         }
-        return true; 
-    }
-}
+        return true;
 
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();  
-        String str = "ABCDCBA";  
-
-        // Check if the string is a palindrome using the Solution class
-        boolean ans = solution.isPalindrome(str);
-
-        // Output the result
-        if (ans) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
-        }
     }
 }
